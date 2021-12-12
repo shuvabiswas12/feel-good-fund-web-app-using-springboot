41b25947bbcb9892f7001d1dcdccc7ef22ef038d
Shuva_Biswas <shuvabiswas12@gmail.com>
2021-12-12 21:28:57 +0600

    login succesful.

--- src/main/java/com/feelGoodFundBudgetingSystem/feelgoodfund/users/LoginController.java
+++ src/main/java/com/feelGoodFundBudgetingSystem/feelgoodfund/users/LoginController.java
@@ -4,11 +4,15 @@ import com.feelGoodFundBudgetingSystem.feelgoodfund.DBInstance;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.concurrent.ExecutionException;
 
@@ -23,13 +27,24 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String submitLogin(@ModelAttribute("user") Users user) throws ExecutionException, InterruptedException {
+    public ModelAndView submitLogin(@ModelAttribute("user") Users user) throws ExecutionException, InterruptedException {
         Firestore dbFireStore = DBInstance.getDBInstance();
-//        ApiFuture<DocumentSnapshot> apiFuture = dbFireStore.document("users/email").get();
-//        // .get() blocks on response
-//        DocumentSnapshot documentSnapshot = apiFuture.get();
-//        System.out.println(documentSnapshot.toObject(Users.class));
-//        return documentSnapshot.toObject(Users.class);
-        return "redirect:/home";
+
+        ApiFuture<DocumentSnapshot> apiFuture = dbFireStore.document("users/"+user.getEmail()).get();
+        // .get() blocks on response
+        DocumentSnapshot documentSnapshot = apiFuture.get();
+        Users foundExistingUser = documentSnapshot.toObject(Users.class);
+        if (user.getPassword().toString().equals(foundExistingUser.getPassword().toString())) {
+            System.out.println("Credentials matched");
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("home");
+            return mv;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Wrong credentials! Try again.");
+        mv.setViewName("login");
+
+        return mv;
     }
 }
